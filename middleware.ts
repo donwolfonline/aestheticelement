af68bf4d4878ec85612fe53c46b7f3a2ab4847cd
Frederick D. Dineen <19985146+donwolfonline@users.noreply.github.com>
2026-02-15 13:15:21 +0300

    chore: set default locale to Arabic

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
     locales: ['ar', 'en'],
     defaultLocale: 'ar',
+    localeDetection: false
 });
 
 export const config = {
